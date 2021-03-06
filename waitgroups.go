d19b33e43ed8f3e90d586afcf1c086e0e8a3e308
Tri Budiyono <tribudiyono39@gmail.com>
2021-03-06 15:38:49 +0700

    another basic golang

---
+++ waitgroups.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker2(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done \n", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; 1 <= 5; i++ {
+		wg.Add(1)
+		go worker2(i, &wg)
+	}
+	wg.Wait()
+}
